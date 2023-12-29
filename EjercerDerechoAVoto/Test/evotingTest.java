a4e6abdd827fe546b22c095223913befb303145d
Quim <quimsanchezbenet@gmail.com>
2023-12-29 19:52:08 +0100

    5 new tests

--- EjercerDerechoAVoto/Test/evotingTest.java
+++ EjercerDerechoAVoto/Test/evotingTest.java
@@ -1,10 +1,13 @@
 import data.Nif;
+import data.Password;
 import data.biometricaldataperipherial.SingleBiometricData;
 import evoting.votingKiosk;
 import exceptions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import services.*;
+
+import java.net.ConnectException;
 
 import static org.junit.Assert.*;
 
@@ -42,6 +45,34 @@ public class evotingTest implements TestingIntf{
         vKiosk.setDocument('d');
         assertEquals(vKiosk.getCurrentPhase(), 3);
     }
+
+    @Test
+    public void enterAccountTest() throws ProceduralException, PasswordIsNullException, InvalidAccountException {
+        Password testPassword = new Password("1234");
+        vKiosk.enterAccount("usertest", testPassword);
+        assertEquals(vKiosk.getCurrentPhase(), 4);
+    }
+
+    @Test
+    public void confirmIdetifTest() throws ProceduralException, InvalidDNIDocumException {
+
+        vKiosk.confirmIdentif('C');
+        assertEquals(vKiosk.getCurrentPhase(), 5);
+    }
+
+    @Test
+    public void enterNifTest() throws ProceduralException, NifIsNullException, NifNotValidException, NotEnabledException, ConnectException {
+        Nif nif = new Nif("11111111S");
+        vKiosk.enterNif(nif);
+        assertEquals(vKiosk.getCurrentPhase(), 6);
+    }
+    @Test
+    public void initOptionNavegationTest() throws ProceduralException {
+        vKiosk.initOptionsNavigation();
+        assertEquals(vKiosk.getCurrentPhase(), 7);
+    }
+
+
 
 
 }
